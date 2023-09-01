e9fa086df301b7f34d0710839302fa9277014991
Jair Acevedo <jairjoseacevedomateus123@hotmail.com>
2023-09-01 18:35:39 -0500

    Primer commit - Interfaz

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <h1 class="titulo-tienda">K-store</h1><br>
+        </div>
+        <div class="carrito">
+            <img src="img/carritocompras.png" alt="carrito" class="logo-carrito">
+            <p class="texto-carrito">[0]</p>
+        </div>
+    </header>
+    <div id="contenedor" class="contenedor">
+        <div>
+            <img src="img/albumkpop.jpg" alt="Imagen" class="imagen-producto">
+            <div class="info-producto">
+                <h2>Stray Kids</h2>
+                <p class="precio">$25.00</p>
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+
+        <div>
+            <img src="img/albumkpop2.jpg" alt="Imagen" class="imagen-producto">
+            <div class="info-producto">
+                <h2>Twice</h2>
+                <p class="precio">$25.00</p>
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+
+        <div>
+            <img src="img/albumkpop3.jpg" alt="Imagen" class="imagen-producto">
+            <div class="info-producto">
+                <h2>Seventeen</h2>
+                <p class="precio">$25.00</p>
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+
+        <div>
+            <img src="img/albumkpop3.jpg" alt="Imagen" class="imagen-producto">
+            <div class="info-producto">
+                <h2>Seventeen</h2>
+                <p class="precio">$25.00</p>
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+        <div>
+            <img src="img/albumkpop2.jpg" alt="Imagen" class="imagen-producto">
+            <div class="info-producto">
+                <h2>Twice</h2>
+                <p class="precio">$25.00</p>
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+        <div>
+            <img src="img/albumkpop.jpg" alt="Imagen" class="imagen-producto">
+            <div class="info-producto">
+                <h2>Stray Kids</h2>
+                <p class="precio">$25.00</p>
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+    </div>
+    <br>
+</body>
+</html>
